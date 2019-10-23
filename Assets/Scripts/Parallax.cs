4155555219e4feb677006386f87d9a5e85832fad
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2019-10-23 18:42:28 +0100

    Something working

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Camera cam = null;
+    public float movementFactor = 1.0f;
+    public bool verticalDisplacement = false;
+    private float width = 0.0f;
+    private float height = 0.0f;
+    private Vector2 startPosition;
+
+    void Awake() {
+        if (cam == null) {
+            cam = Camera.main;
+        }
+
+        startPosition = new Vector2(transform.position.x, transform.position.y);
+
+        Vector3 size = GetComponent<SpriteRenderer>().bounds.size;
+        width = size.x;
+        height = size.y;
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float horizontalDist = cam.transform.position.x * movementFactor;
+
+        if (horizontalDist >= width + transform.position.x) {
+            transform.position += new Vector3(horizontalDist + width, 0.0f, 0.0f); 
+        }
+        if (horizontalDist >= width - transform.position.x) {
+            transform.position -= new Vector3(horizontalDist + width, 0.0f, 0.0f); 
+        }
+
+        if (verticalDisplacement) {
+            float verticalDist = cam.transform.position.y * movementFactor;
+
+            if (verticalDist >= height + transform.position.y) {
+                transform.position += new Vector3(0.0f, verticalDist + height, 0.0f); 
+            }
+            if (verticalDist >= height - transform.position.x) {
+                transform.position -= new Vector3(0.0f, verticalDist + height, 0.0f); 
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Camera cam = null;
+    public float movementSpeed = 1.0f;
+    public float cameraMaxSeekDistance = .5f;
+    private Animator animator;
+
+
+    void Awake() {
+        if (cam == null) {
+            cam = Camera.main;
+        }
+
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
+        animator.SetFloat("Speed", Mathf.Abs(vertical + horizontal));
+        
+        // Character movement
+        transform.position += 
+            new Vector3(horizontal, vertical, 0.0f).normalized * movementSpeed * Time.deltaTime;
+
+        Vector2 camPos = new Vector2(cam.transform.position.x, cam.transform.position.y);
+        Vector2 playerPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 smoothedCameraPosition = 
+            Vector2.Lerp(camPos, playerPos, 
+                0.0525f
+                //Mathf.Clamp(Vector2.Distance(camPos, playerPos) - cameraMaxSeekDistance, 0, 1)
+            );
+
+        cam.transform.position = 
+            new Vector3(smoothedCameraPosition.x, smoothedCameraPosition.y, cam.transform.position.z);
+    }
+}
