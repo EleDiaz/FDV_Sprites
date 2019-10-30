e6958ef1409030cf9bb776da7215f38dd85648bd
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2019-10-30 16:26:51 +0000

    Adding some physics

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    public float jumpForce = 2.0f;
+
+    void OnTriggerEnter2D(Collider2D collider2D) {
+        Rigidbody2D rb = collider2D.GetComponent<Rigidbody2D>();
+        if (rb != null) {
+            rb.AddForce(jumpForce * -transform.up, ForceMode2D.Impulse);
+        }
+    }
+}
--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -21,6 +21,9 @@ public class Parallax : MonoBehaviour
         Vector3 size = GetComponent<SpriteRenderer>().bounds.size;
         width = size.x;
         height = size.y;
+        Debug.Log(size);
+        Debug.Log("Start Pos" + startPosition);
+        Debug.Log("Cam Pos" + cam.transform.position);
     }
 
     void Start()
@@ -30,23 +33,41 @@ public class Parallax : MonoBehaviour
     void Update()
     {
         float horizontalDist = cam.transform.position.x * movementFactor;
+        float verticalDist = cam.transform.position.y * movementFactor;
+        float hTemp = (cam.transform.position.x * (1 - movementFactor));
+        float vTemp = (cam.transform.position.y * (1 - movementFactor));
+        
+        // width >= abs(cam.transform.position.x * movementFactor - transform.position.x) // reset position
+        // reset position transform.position.x += width * movementFactor
+        transform.position = new Vector3(startPosition.x + horizontalDist, startPosition.y + verticalDist, transform.position.z);
+        if (hTemp > startPosition.x + width) startPosition.x += width;
+        else if (hTemp < startPosition.x - width) startPosition.x -= width;
 
-        if (horizontalDist >= width + transform.position.x) {
-            transform.position += new Vector3(horizontalDist + width, 0.0f, 0.0f); 
-        }
-        if (horizontalDist >= width - transform.position.x) {
-            transform.position -= new Vector3(horizontalDist + width, 0.0f, 0.0f); 
-        }
+        if (vTemp > startPosition.y + height) startPosition.y += height;
+        else if (vTemp < startPosition.y - height) startPosition.y -= height;
 
-        if (verticalDisplacement) {
-            float verticalDist = cam.transform.position.y * movementFactor;
+        // if (width / 2 <= cam.transform.position.x - transform.position.x) {
+        //     Debug.Log("changed width");
+        //     // Debug.Log("Distance" + (cam.transform.position.x * movementFactor - transform.position.x));
+        //     transform.position += new Vector3(width, 0.0f, 0.0f); 
+        //     startPosition.x += width;
+        // }
+        // if (-width/2 >= cam.transform.position.x - transform.position.x) {
+        //     Debug.Log("changed -width");
+        //     // Debug.Log("Distance " + (cam.transform.position.x * movementFactor - transform.position.x));
+        //     //transform.position -= new Vector3(horizontalDist + width, 0.0f, 0.0f); 
+        //     startPosition.x -= width;
+        // }
 
-            if (verticalDist >= height + transform.position.y) {
-                transform.position += new Vector3(0.0f, verticalDist + height, 0.0f); 
-            }
-            if (verticalDist >= height - transform.position.x) {
-                transform.position -= new Vector3(0.0f, verticalDist + height, 0.0f); 
-            }
-        }
+        // if (verticalDisplacement) {
+        //     float verticalDist = cam.transform.position.y * movementFactor;
+
+        //     if (verticalDist >= height + transform.position.y) {
+        //         transform.position += new Vector3(0.0f, verticalDist + height, 0.0f); 
+        //     }
+        //     if (verticalDist >= height - transform.position.x) {
+        //         transform.position -= new Vector3(0.0f, verticalDist + height, 0.0f); 
+        //     }
+        // }
     }
 }
